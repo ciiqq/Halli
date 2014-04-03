cb3e86485a8441758cc248049f5cf8f919b7f8f3
moijari <a1103693@myy.haaga-helia.fi>
2014-04-03 09:12:05 +0300

    controller lisätty

--- Halli/src/main/java/fi/softala/controller/Controller1.java
+++ Halli/src/main/java/fi/softala/controller/Controller1.java
@@ -1,13 +1,22 @@
 package fi.softala.controller;
 
+import java.util.List;
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
 
+
+import fi.softala.bean.Aikatauluslotti;
+import fi.softala.DAO.AikatauluslottiDAO;
 
 @Controller
 @RequestMapping (value="/testaus")
 public class Controller1 {
+	@Inject
+	AikatauluslottiDAO dao;
 	
 	@RequestMapping(value="uusi", method=RequestMethod.GET)
 	public String getCreateForm() {
@@ -18,5 +27,17 @@ public class Controller1 {
 	public String kouluttajienLisaysSivu() {
 		return "kouluttajien_lisays/lisays";
 	}
+
+	@RequestMapping(value="aikatauluslottilista2", method=RequestMethod.GET)
+	public String aikatauluslottiLista(Model model) { /* JariK 20140319 */
+		List<Aikatauluslotti> aikatauluslotit = dao.haeKaikki();
+ 		model.addAttribute("aikatauluslotit", aikatauluslotit);
+		return "aikatauluslottilista";
+	}
+	@RequestMapping(value="kalenteri", method=RequestMethod.GET)
+	public String kalenteri(Model model) { /* JariK 20140319 */
+		return "kalenteri";
+	}
+	
 }
 
