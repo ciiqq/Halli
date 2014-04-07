da53742ae5ea6c2b46af55a42bd97f485d81dead
jptoura <jptoura@iki.fi>
2014-04-07 10:19:17 +0300

    .gitignore testing

--- Halli/src/main/java/fi/softala/controller/KoulutusController.java
+++ Halli/src/main/java/fi/softala/controller/KoulutusController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping (value="/koulutus")
+@RequestMapping (value="/koulutus")// TODO
 public class KoulutusController {
 	
 	@RequestMapping (value="/uusi", method=RequestMethod.GET)
