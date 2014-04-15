639d3d7b936e95da80a3a1a2fd43ebd60f30bccc
Samu.Mukkala <samumukkala@gmail.com>
2014-04-15 12:55:41 +0300

    testaus säätöä

--- Halli/src/test/java/fi/softala/koulutuslistaus/KoulutuslistausTest.java
+++ Halli/src/test/java/fi/softala/koulutuslistaus/KoulutuslistausTest.java
@@ -2,6 +2,13 @@ package fi.softala.koulutuslistaus;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import fi.softala.bean.Aikatauluslotti;
@@ -9,13 +16,42 @@ import fi.softala.dao.KoulutusDAO;
 
 public class KoulutuslistausTest {
 	
+	@BeforeClass //Ajetaan ennen kaikkia testejä.  Tänne voi laittaa alustukset, jotka ovat kaikille testeille yhteiset
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass //Ajetaan kaikkien testien jälkeen
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before //Ajetaan ennen jokaista testiä
+    public void setUp() {
+    }
+
+    @After //Ajetaan jokaisen testin jälkeen
+    public void tearDown() {
+    }
+	
 	
 	
 	@Test
-	public void testaaKoulutustenHaku(){
+	public void testaaKoulutuksenHaku(){
 		KoulutusDAO test = new MockKoulutus();
 		Aikatauluslotti koulutusTest = test.haeKoulutus(1);
-		assertNotNull(koulutusTest);
+		assertNotNull(koulutusTest);		
+	}
+	
+	@Test
+	public void testaaKoulutusListaus(){
+		
+		KoulutusDAO test = new MockKoulutus();		
+		
+		List<Aikatauluslotti> actual = test.haeKoulutukset();
+		List<String> expected = Arrays.asList("fee", "fi", "foe");		
+			
+	
+		
+		assertEquals(actual, expected);            
 		
 		
 	}
