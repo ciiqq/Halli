4419dc95f6dfd28b358bfe1c6bfe7780880f0698
Aleksi Monaco <aleksi.monaco@myy.haaga-helia.fi>
2014-04-15 12:23:42 +0300

    nimi väärin

--- Halli/src/test/java/fi/softala/koulutuslistaus/KoulutuslistausTest.java
+++ Halli/src/test/java/fi/softala/koulutuslistaus/KoulutuslistausTest.java
@@ -2,13 +2,45 @@ package fi.softala.koulutuslistaus;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import fi.softala.bean.Aikatauluslotti;
 import fi.softala.dao.KoulutusDAO;
+import fi.softala.dao.KoulutusRowMapper;
 
 public class KoulutuslistausTest {
 	
+	@Inject
+	private KoulutusDAO dao;
+	
+	public KoulutusDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(KoulutusDAO dao) {
+		this.dao = dao;
+	}
+
+	@Test
+	public void koulutusListausTest(){
+
+		//List<Aikatauluslotti> koulutuslista = dao.haeKoulutukset();
+		
+		List<Aikatauluslotti> testilista = new ArrayList<Aikatauluslotti>();
+		
+		String lol = "moi";
+		
+		assertNotNull(lol);
+	}
 	
 	
 	@Test
