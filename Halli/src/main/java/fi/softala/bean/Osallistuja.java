56723eba8d049943040f420fb47ea85b812e512a
Henna Kiiveri <hennakiiveri@gmail.com>
2014-04-22 11:27:08 +0300

    Opiskelijanumeron muotoilu kantaa varten + sähköpostiosoitteen muotoilu maileria varten

--- Halli/src/main/java/fi/softala/bean/Osallistuja.java
+++ Halli/src/main/java/fi/softala/bean/Osallistuja.java
@@ -6,6 +6,7 @@ public class Osallistuja {
 	private String opiskelijanro;
 	private String etunimi;
 	private String sukunimi;
+	private String sahkoposti;
 	
 	public Osallistuja() {
 		id = 0;
@@ -36,7 +37,13 @@ public class Osallistuja {
 	}
 
 	public String getOpiskelijanro() {
-		return opiskelijanro;
+		if (opiskelijanro.length() == 8){
+			opiskelijanro = opiskelijanro.substring(0, 7);
+			return opiskelijanro;
+		}
+		else{
+			return opiskelijanro;
+		}
 	}
 
 	public void setOpiskelijanro(String opiskelijanro) {
@@ -57,6 +64,14 @@ public class Osallistuja {
 
 	public void setSukunimi(String sukunimi) {
 		this.sukunimi = sukunimi;
+	}
+	
+	public void setSahkoposti(String etunimi, String sukunimi){
+		 sahkoposti = etunimi + "." + sukunimi + "@myy.haaga-helia.fi";
+	}
+	
+	public String getSahkoposti(){
+		return sahkoposti;
 	}
 
 	@Override
