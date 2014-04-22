b43477f3e797edc6d647989a6bc3b4743bb1076c
Juhani Niinimaa <juhani.niinimaa@myy.haaga-helia.fi>
2014-04-22 08:22:34 +0300

    tämä on testikenttä controlleriin.

--- Halli/src/main/java/fi/softala/controller/KoulutusController.java
+++ Halli/src/main/java/fi/softala/controller/KoulutusController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import fi.softala.bean.Aikatauluslotti;
 import fi.softala.dao.KoulutusDAO;
 
-
+/*
+ * Testi
+ */
 
 @Controller
 public class KoulutusController {
