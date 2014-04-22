bbd8dafc6d33307b6764da75c55ca1d0fa75b992
OskariN <thunderelement4l@gmail.com>
2014-04-22 10:09:58 +0300

    salasanageneraattori
    generoi salasanan

---
+++ Halli/src/main/java/fi/softala/bean/SalasanaGeneraattori.java
@@ -0,0 +1,14 @@
+package fi.softala.bean;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public abstract class SalasanaGeneraattori {
+
+	public static String generoiSalasana(int pituus){
+		return RandomStringUtils.randomAlphanumeric(pituus);
+	}
+	
+	public static String generoiSalasana(){
+		return RandomStringUtils.randomAlphanumeric(8);
+	}
+}
