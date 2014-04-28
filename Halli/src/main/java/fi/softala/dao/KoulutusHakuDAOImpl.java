b039edfd685e65e3b66b0bec1bff11e3f2f9c4e8
jirikuusela <jiri.kuusela@gmail.com>
2014-04-28 14:35:28 +0300

    Junit testailua varten muutoksia.

--- Halli/src/main/java/fi/softala/dao/KoulutusHakuDAOImpl.java
+++ Halli/src/main/java/fi/softala/dao/KoulutusHakuDAOImpl.java
@@ -33,13 +33,13 @@ public class KoulutusHakuDAOImpl implements KoulutusHakuDAO {
 	public List<Koulutustilaisuus> haeKaikki() {
 		String sql = "SELECT k.*, ast.*, ko.opiskelijanro, ko.etunimi AS etunimi, ko.sukunimi AS sukunimi, 1 kouluttaja_true, '' AS avainsana "
 				+ "FROM koulutustilaisuus k "
-				+ "JOIN koulutuksenKouluttaja kk ON k.koulutus_id = kk.koulutus_id "
+				+ "JOIN koulutuksenkouluttaja kk ON k.koulutus_id = kk.koulutus_id "
 				+ "JOIN kouluttaja ko ON ko.opiskelijanro = kk.opiskelijanro "
 				+ "JOIN aikatauluslotti ast ON ast.koulutus_id = k.koulutus_id "
 				+ "UNION ALL "
 				+ "SELECT k.*, ast.*, '', '', '', 0 kouluttaja_true, a.avainsana "
 				+ "FROM koulutustilaisuus k "
-				+ "JOIN koulutuksenAvainsana ka ON ka.koulutus_id = k.koulutus_id "
+				+ "JOIN koulutuksenavainsana ka ON ka.koulutus_id = k.koulutus_id "
 				+ "JOIN avainsana a ON a.avainsana_id = ka.avainsana_id "
 				+ "JOIN aikatauluslotti ast ON ast.koulutus_id = k.koulutus_id;";
 		List<Koulutustilaisuus> koulutukset = jt.query(sql,
@@ -50,14 +50,14 @@ public class KoulutusHakuDAOImpl implements KoulutusHakuDAO {
 	public List<Koulutustilaisuus> haeVahvistamattomat(){
 		String sql = "SELECT k.*, ast.*, ko.opiskelijanro, ko.etunimi AS etunimi, ko.sukunimi AS sukunimi, 1 kouluttaja_true, '' AS avainsana "
 				+ "FROM koulutustilaisuus k "
-				+ "JOIN koulutuksenKouluttaja kk ON k.koulutus_id = kk.koulutus_id "
+				+ "JOIN koulutuksenkouluttaja kk ON k.koulutus_id = kk.koulutus_id "
 				+ "JOIN kouluttaja ko ON ko.opiskelijanro = kk.opiskelijanro "
 				+ "JOIN aikatauluslotti ast ON ast.koulutus_id = k.koulutus_id "
 				+ "WHERE k.nakyvyys = 0 "
 				+ "UNION ALL "
 				+ "SELECT k.*, ast.*, '', '', '', 0 kouluttaja_true, a.avainsana "
 				+ "FROM koulutustilaisuus k "
-				+ "JOIN koulutuksenAvainsana ka ON ka.koulutus_id = k.koulutus_id "
+				+ "JOIN koulutuksenavainsana ka ON ka.koulutus_id = k.koulutus_id "
 				+ "JOIN avainsana a ON a.avainsana_id = ka.avainsana_id "
 				+ "JOIN aikatauluslotti ast ON ast.koulutus_id = k.koulutus_id "
 				+ "WHERE k.nakyvyys = 0; ";
