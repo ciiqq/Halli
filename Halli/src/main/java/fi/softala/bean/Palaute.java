8c5eb27ad6df534ffbd73fe61a36bfd56a1b7d73
tommileskinenmyyhaaga-heliafi <tommi.leskinen@myy.haaga-helia.fi>
2014-05-08 08:46:32 +0300

    Palaute bean muutettu
    palaute bean muutettu

---
+++ Halli/src/main/java/fi/softala/bean/Palaute.java
@@ -0,0 +1,50 @@
+package fi.softala.bean;
+
+public class Palaute {
+
+	private int palaute_id;
+	private int arvosana;
+	private String palauteteksti;
+	private String opiskelijanumero;
+	
+	public String getOpiskelijanumero() {
+		return opiskelijanumero;
+	}
+
+	public void setOpiskelijanumero(String opiskelijanumero) {
+		this.opiskelijanumero = opiskelijanumero;
+	}
+
+	public int getPalaute_id() {
+		return palaute_id;
+	}
+
+	public void setPalaute_id(int palaute_id) {
+		this.palaute_id = palaute_id;
+	}
+
+	public int getArvosana() {
+		return arvosana;
+	}
+
+	public void setArvosana(int arvosana) {
+		this.arvosana = arvosana;
+	}
+
+	public String getPalauteteksti() {
+		return palauteteksti;
+	}
+
+	public void setPalauteteksti(String palauteteksti) {
+		this.palauteteksti = palauteteksti;
+	}
+	
+	@Override
+	public String toString() {
+		return "Palaute [palaute_id=" + palaute_id + ", arvosana=" + arvosana
+				+ ", palauteteksti=" + palauteteksti + ", opiskelijanumero="
+				+ opiskelijanumero + "]";
+	}
+	
+	
+}
